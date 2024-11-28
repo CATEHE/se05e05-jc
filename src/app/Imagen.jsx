eaaa0af8595da41a1c76e8599b1d0bae95a692f1
CATEHETeam <catehe.inc@gmail.com>
2024-11-27 18:30:51 -0600

    imagen

--- src/app/Imagen.jsx
+++ src/app/Imagen.jsx
@@ -4,7 +4,7 @@ import '../styles/img_portada.css'
 const Imagen = () => {
   return (
     <div>
-      <img className='img_portada' src='../../public/pokemon2.jpg' alt="logo pokemon" />
+      <img className='img_portada' src='https://lepton-tronic.com/images/pokemon2.jpg' alt="logo pokemon" />
     </div>
   );
 };
